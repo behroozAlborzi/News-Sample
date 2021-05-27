6899407f26f309efce2800225743c46f9fa58017
behrooz alborzi <behrouz.alborzi@gmail.com>
2021-05-27 23:44:55 +0430

    first commit: create files

---
+++ app/src/main/java/com/behroozalborzi/newssample/MainActivity.java
@@ -0,0 +1,14 @@
+package com.behroozalborzi.newssample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
